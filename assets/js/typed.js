3b8a0bb9ca8d2a61ac50bd91b0b449129c638002
hst1189 <hst1189@hotmail.com>
2025-09-21 18:58:50 +0900

    update

---
+++ assets/js/typed.js
@@ -0,0 +1,19 @@
+var typed = new Typed('#typed', {
+	strings: ["《LIFE OF JOHN KEATS》--威廉 ·邁克爾 · 羅塞蒂",
+		      "I love three things in this world.",
+		      "Sun, moon and you.",
+		      "Sun for morning, moon for night, and you forever.",
+		      "浮世萬千，吾愛有三。",
+		      "日，月，與卿。",
+		      "日爲朝，月爲暮，卿爲朝朝暮暮。",
+		      "宋詞《鵲橋仙·纖雲弄巧》--秦觀",
+		      "纖雲弄巧，飛星傳恨，銀漢迢迢暗度。",
+		      "金風玉露一相逢，便勝卻人間無數。",
+		      "柔情似水，佳期如夢，忍顧鵲橋歸路。",
+		      "兩情若是久長時，又豈在朝朝暮暮。"], 
+    // stringsElement: '#info_box',
+    typeSpeed: 200,//打字的速度
+    backSpeed: 100,
+    startDelay: 300, // 开始之前的延迟300毫秒
+    loop: true // 是否循环
+  });
